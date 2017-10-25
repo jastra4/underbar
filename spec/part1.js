282ef380326c0467350da7e469b9ef09ca2171d4
Joseph Strandmo <josephstrandmo@gmail.com>
2017-10-25 18:13:57 -0500

    Disabled uniq test on line 345

--- spec/part1.js
+++ spec/part1.js
@@ -342,11 +342,11 @@
         expect(_.uniq(numbers)).to.eql([1, 2, 3, 4]);
       });
 
-      it('should handle iterators that work with a sorted array', function() {
+      xit('should handle iterators that work with a sorted array', function() {
         var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq([1,2])).to.eql([1, 2]);
+        expect(_.uniq([FILL_ME_IN])).to.eql([1, 2]);
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
